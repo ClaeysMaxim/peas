d3b205ce84a318c310780ab8cc57f0f93408cf69
ClaeysMaxim <maxim.claeys@student.arteveldehs.be>
2025-06-20 16:51:35 +0200

    Refactor ServicesSection component to use TanStack Router instead of React Router

--- src/components/homepage/ServicesSection.jsx
+++ src/components/homepage/ServicesSection.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link } from "@tanstack/react-router";
 import { FaPlane, FaCarAlt, FaUsers } from "react-icons/fa";
 import { useTranslation } from "../../hooks/useTranslation";
 
